c4344639c5f77b22a4c84950f837b5a6417859e5
Hasan Zawahra <hszawa1985@gmail.com>
2024-02-19 22:30:51 +0200

    more complix

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,11 +1,17 @@
-const colors = ["red", "blue", "green", "white", "orange", "yellow", "pink"];
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("btn");
 const body = document.getElementsByTagName("body")[0];
 const span = document.getElementById("sp");
 
 btn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * (colors.length));
-    body.style.backgroundColor = colors[randomIndex];
-    span.innerHTML = colors[randomIndex];
+    var hex = "#";
+
+    for (let i = 0; i < 6; i++){
+        const randomIndex = Math.floor(Math.random() * (colors.length));
+        hex += colors[randomIndex];
+    }
+
+    body.style.backgroundColor = hex;
+    span.innerHTML = hex;
 });
