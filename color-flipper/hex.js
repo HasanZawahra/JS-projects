71177b8a04492ad09cce9e2cbc9817a312d88039
Hasan Zawahra <hszawa1985@gmail.com>
2024-02-19 21:47:37 +0200

    first commit

---
+++ color-flipper/hex.js
@@ -0,0 +1,11 @@
+const colors = ["red", "blue", "green", "white", "orange", "yellow", "pink"];
+
+const btn = document.getElementById("btn");
+const body = document.getElementsByTagName("body")[0];
+const span = document.getElementById("sp");
+
+btn.addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * (colors.length-1));
+    body.style.backgroundColor = colors[randomIndex];
+    span.innerHTML = colors[randomIndex];
+});
